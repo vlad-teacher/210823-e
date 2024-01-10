1d953f746ba9c32706d9ae4d55eb9955e88310d6
Nastya.Paliy <anastasiapaliy97@gmail.com>
2024-01-10 20:41:40 +0100

    classwork

--- Anastasiia Palii/classwork/task1/index.js
+++ Anastasiia Palii/classwork/task1/index.js
@@ -3,30 +3,35 @@
 // 2.2 массив в который войдут только те числа которые повторялись хотя бы один раз [2, 4]
 
 
-const inputArray = [2, 4, 5, 6, 1, 2, 7, 3, 4];
-const arr = [];
+const arr1 = [2, 4, 2, 2, 5, 6, 1, 2, 7, 3, 4];
 
-// 2.1
-const uniqueNumbers = [];
 
-for (const num of inputArray) {
-    if (!arr.includes(num)) {
-        arr.push(num);
-        uniqueNumbers.push(num);
-    } else {
-        const index = arr.indexOf(num);
-        arr.splice(index, 1);
+function getUnicElements(arr) { // [2, 4, 2, 2, 5, 6, 1, 2, 7, 3, 4]
+    const unicElem = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.lastIndexOf(arr[i]) === arr.indexOf(arr[i])) {
+            unicElem.push(arr[i]);
+        }
     }
+
+    return unicElem;
 }
 
+function getDublicateElements(arr) {
+    const dublicateElem = [2];
 
-// 2.2
-const repeatedNumbers = [];
-
-for (const num of inputArray) {
-    if (!arr.includes(num)) {
-        arr.push(num);
-    } else if (!repeatedNumbers.includes(num)) {
-        repeatedNumbers.push(num);
+    for (let i = 0; i < arr.length; i++) {
+        if (
+            arr.lastIndexOf(arr[i]) !== arr.indexOf(arr[i]) && // [2, 4, 2, 2, 5, 6, 1, 2, 7, 3, 4]
+            !dublicateElem.includes(arr[i])
+        ) {
+            dublicateElem.push(arr[i]);
+        }
     }
+
+    return dublicateElem;
 }
+
+const result = getUnicElements(arr1);
+const result2 = getUnicElements([2,4,1,4,523,543,5,45,352,342,4,4]);
