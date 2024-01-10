376a3008786a2657fc3fe169b77e8908f6231098
Katja <asrounk@gmail.com>
2024-01-10 20:39:16 +0100

    I changed

--- Katja Asroun/classwork/task1/index.js
+++ Katja Asroun/classwork/task1/index.js
@@ -2,9 +2,35 @@
 // 2.1 массив в который войдут только те числа которые не повторялись ни одного раза [5, 6, 1, 7, 3]
 // 2.2 массив в который войдут только те числа которые повторялись хотя бы один раз [2, 4]
 
-let arr = [2,4,5,6,1,2,7,3,4];
+const arr = [2, 4, 2, 2, 5, 6, 1, 2, 7, 3, 4];
 
-const set = new Set (arr);
-const arrNew = [...set];
 
-console.log(arrNew);
+function getUnicElements(arr) { // [2, 4, 2, 2, 5, 6, 1, 2, 7, 3, 4]
+    const unicElem = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.lastIndexOf(arr[i]) === arr.indexOf(arr[i])) {
+            unicElem.push(arr[i]);
+        }
+    }
+
+    return unicElem;
+}
+
+function getDublicateElements(arr) {
+    const dublicateElem = [2];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (
+            arr.lastIndexOf(arr[i]) !== arr.indexOf(arr[i]) && // [2, 4, 2, 2, 5, 6, 1, 2, 7, 3, 4]
+            !dublicateElem.includes(arr[i])
+        ) {
+            dublicateElem.push(arr[i]);
+        }
+    }
+
+    return dublicateElem;
+}
+
+const result = getUnicElements(arr);
+const result2 = getUnicElements([2,4,1,4,523,543,5,45,352,342,4,4]);
