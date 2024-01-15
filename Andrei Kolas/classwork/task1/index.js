16c73ba32fb6eebf31ae0a18b6c4cd6e9fc83dac
Andrei.Kolas <andreysamp228@gmail.com>
2024-01-15 03:48:15 +0100

    classwork task № 2

--- Andrei Kolas/classwork/task1/index.js
+++ Andrei Kolas/classwork/task1/index.js
@@ -8,14 +8,33 @@ const arr228 = [2,4,5,6,1,2,7,3,4];
 
 function getUniqueElem (arr) {
 
-   const UniqueElem = [];
+   const uniqueElem = [];
    for (let i = 0; i < arr.length; i++) {
       if (arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])) {
-         UniqueElem.push(arr[i]);
+         uniqueElem.push(arr[i]);
       }
    }
-   return UniqueElem;
+   return uniqueElem;
+}
+
+
+
+function getDuplicateElem (arr) {
+
+   const duplicateElem = [];
+   for (let i = 0; i < arr.length; i++) {
+
+      if (duplicateElem.includes(arr[i])) {
+         continue
+      }
+      if (arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) {
+         duplicateElem.push(arr[i])
+      }
+      
+   }
+   return duplicateElem;
 }
 
 
 console.log(getUniqueElem(arr228));
+console.log(getDuplicateElem(arr228));
