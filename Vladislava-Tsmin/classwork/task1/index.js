94837b744eef514e0f72a06b624a9a8dd0811322
vlada <vladislava568cmin569@gmail.com>
2024-01-21 11:34:31 +0100

    hw10.01

--- Vladislava-Tsmin/classwork/task1/index.js
+++ Vladislava-Tsmin/classwork/task1/index.js
@@ -32,4 +32,40 @@ function getDublicateElements(arr) {
 }
 
 const result = getUnicElements(arr1);
-const result2 = getUnicElements([2,4,1,4,523,543,5,45,352,342,4,4]);
+const result2 = getUnicElements([2, 4, 1, 4, 523, 543, 5, 45, 352, 342, 4, 4]);
+
+
+//17.01
+const arr2 = [
+    {
+        name: "Valerchik",
+        age: 23,
+    },
+    {
+        name: "Olejik",
+        age: 90,
+    },
+    {
+        name: "John",
+        age: 12,
+    },
+    {
+        name: "Mitrofan",
+        age: 35,
+    },
+];
+
+// 1. проверить есть ли в массиве объект у которого имя митрофан (some, find)
+// 2. проверить всем ли есть 18 лет
+// 3. с помощью метода filter оставить в массиве только те объекты у которых длина имени больше 6
+// 4. с помощью меотда reduce Сформировать строку вида 'Hello Valerchik and Olejik ...'
+
+const mitrofan = arr2.find(({ name }) => name === 'Mirtofan')
+console.log(mitrofan);
+
+const isOver18 = arr2.every(({ age }) => age > 18);
+if (isOver18) {
+    console.log('Всем больше 18');
+} else {
+    console.log('Косу то меньше 18');
+}
