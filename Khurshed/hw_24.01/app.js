6c59e290c99ed13fb1099d138a22430e6ac5f348
Khurshed007 <khurshed.ahmadjonov@bk.ru>
2024-02-09 09:33:38 +0100

    mychanges

--- Khurshed/hw_24.01/app.js
+++ Khurshed/hw_24.01/app.js
@@ -1,207 +1,160 @@
-
-
-
-
-//Написать функцию которая принимает массив строк (имена) и два колбэка. функция добавляет к 
-// каждому элементу массива фамилию "Smith" после чего передает получившийся массив в первый колбэк. 
+//Написать функцию которая принимает массив строк (имена) и два колбэка. функция добавляет к
+// каждому элементу массива фамилию "Smith" после чего передает получившийся массив в первый колбэк.
 // Когда первый колбэк отработает, возвращеное им значение нужно передать во второй колбэк.
 
-// 1ый колбэк: это функция которая принимает массив строк, добавляет к каждой строке "Mr. " и возвращает получившийся массив. 
+// 1ый колбэк: это функция которая принимает массив строк, добавляет к каждой строке "Mr. " и возвращает получившийся массив.
 // 2ой колбэк: функция которая принимает массив строк и выводит через alert() каждый элемент массива.
 
-let array = ["Jaden", "Will"]
+let personDates = ["Jaden", "Will"];
+function addSurname(arr, cb1, cb2) {
+  let getSurname = [];
 
-function foo(arr,cb1,cb2){
-  let emptyMassiv = []        
-    for(let i = 0; i < arr.length; i++) {
-         emptyMassiv[i] = [arr[i]]
-         emptyMassiv[i][i + 1] =  " Smith "
+  arr.forEach((items, index) => {
+    getSurname.push([items]);
+    getSurname[index][index + 1] = " Smith ";
+  });
 
-  }
-    console.log(emptyMassiv)
-       cb1(emptyMassiv,cb2)
-  
+  cb1(getSurname, cb2);
 }
 
-function forCb1(arr,cb2) {
-  let newArr = []
-         for(let i = 0; i < arr.length; i++){
-            arr[i].splice(0,0," Mr ")
-            newArr.push(arr[i].join(""))
-          }
+function addAddress(arr, cb2) {
+  let getAddress = [];
 
+  arr.forEach((items, index, massive) => {
+    massive[index].splice(0, 0, " Mr ");
+    getAddress.push(items.join(""));
+  });
 
-
-     cb2(newArr)
-     console.log(newArr)
+  cb2(getAddress);
+  console.log(getAddress, "in function addAddress");
 }
 
-
-function forCb2(arr) {
-    arr.map((item) => {
-       alert(item)
-    })
+function callByAddress(arr) {
+  arr.map((item) => {
+    alert(item);
+  });
 }
 
-
-console.log(foo(array,forCb1,forCb2))
-
-
+addSurname(personDates, addAddress, callByAddress);
 
 //1. Дан массив [1,1,1,4,4,5,1,6,6,74,74,74,3]. Написать функцию, которая вернет массив только из тех чисел, которые повторялись в исходном.
 // myFuncName([1,1,1,4,4,5,1,6,6,74,74,74,3]) //  [1,4,6,74]
-  let arr1 =  [1,1,1,4,4,5,1,6,6,74,74,74,3];
+let numbers = [1, 1, 1, 4, 4, 5, 1, 6, 6, 74, 74, 74, 3];
 
-   function myFuncName(mass){
-        let uniqueEls = [];
-        let repeatEls = {};
+function myFuncName(mass) {
+  let uniqueEls = [];
+  let repeatEls = {};
 
-        mass.reduce((accu,item) => {
-                  if(!accu[0].includes(item)){
-                      accu[0].push(item)
-                  }else{
-                      accu[1][item]  = item
-                  }
-                  return accu
-        },[uniqueEls,repeatEls])
+  mass.reduce(
+    (accu, item) => {
+      if (!accu[0].includes(item)) {
+        accu[0].push(item);
+      } else {
+        accu[1][item] = item;
+      }
+      return accu;
+    },
+    [uniqueEls, repeatEls]
+  );
 
-        repeatEls = Object.values(repeatEls)
-         console.log(repeatEls)
-         return repeatEls
-   }
+  repeatEls = Object.values(repeatEls);
+  console.log(repeatEls);
+  return repeatEls;
+}
 
-   myFuncName(arr1)
- 
-
-
+myFuncName(numbers);
 
 //2. Cоздать случайный двумерный массив (вложенные массивы разной длины) из 8 элементов. Обработать массив reduce() и вернуть тот массив сумма элементов которого наибольшая.
 
-   let arr2 = [[1,2,3,4], [4,5,6,7], [8,9,10,11],[8,9,10,141],[222,333,444],[11,12,123],[22,3,4,6],[44,55,123]];
+let nestedArray = [
+  [1, 2, 3, 4],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [8, 9, 10, 141],
+  [222, 333, 444],
+  [11, 12, 123],
+  [22, 3, 4, 6],
+  [44, 55, 123],
+];
 
-      let res = arr2.map((item,index) => {
-        return item.reduce((accu,e,i) => {
-              accu += e  
-              return accu 
-              
-        },0)
-      })
-    
-      res = arr2[res.indexOf(Math.max(...res))]
-      console.log(res)    
+let nestedArraySums = nestedArray.map((item, index) => {
+  return item.reduce((accu, e, i) => {
+    accu += e;
+    return accu;
+  }, 0);
+});
 
+let maxSumIndex = nestedArraySums.indexOf(Math.max(...nestedArraySums));
 
-     // 3. Дан случайный массив чисел. С помощью метода .filter() отфильтруйте массив так, чтобы в новый массив вошли только четные двухзначные числа из исходного. 
+let maxSum = nestedArray[maxSumIndex];
+console.log(maxSum, "MaxSum");
 
-      let arr3 = [2,4,5,8,10,12,14,16,18,19,21];
+// 3. Дан случайный массив чисел. С помощью метода .filter() отфильтруйте массив так, чтобы в новый массив вошли только четные двухзначные числа из исходного.
 
-      let res3 = arr3.filter((item) => {
-             if(item > 9 && item % 2 == 0){
-                 return true
-             }
-      })
+let randomNumbers = [2, 4, 5, 8, 10, 12, 14, 16, 18, 19, 21];
 
-      console.log(res3)
-   
- // 4, Дан массив объектов:
+let filteredArray = randomNumbers.filter((item) => {
+  return item > 9 && item % 2 == 0;
+});
 
-      const fbTeam = [
-        {
-        playerName:"Messi",
-        team:"Barcelona"
-        },
-        {
-        playerName:"Suarez",
-        team:"Barcelona"
-        },
-        {
-        playerName:"Ronaldo",
-        team:"Juventus"
-        },
-        {
-        playerName:"Buffon",
-        team:"Juventus"
-        },
-        {
-        playerName: 'Valerchik',
-        team: 'Vedrich97'
-        },
-        {
-        playerName:"Gonsalo",
-        team:"Juventus"
-        },
-        {
-        playerName:"Pedro",
-        team:"Chelsea"
-        },
-       {
-        playerName:"Zuma",
-        team:"Chelsea"
-        },
-        {
-        playerName:"Rico",
-        team:"PSG "
-        }
-    ];
+console.log(filteredArray, "FilteredArray");
 
-    // С помощью метода reduce() преобразуйте его в объект вида:
+// 4, Дан массив объектов:
 
-    // const fbPlayersByTeams = {
-    //     Juventus: [{}, {}, {}],
-    //     PSG: [{}],
-    //     Chelsea: [{}, {}],
-    //     Vedrich97: [{}],
-    //     Barcelona: [{}, {}]
-    //  }
-    // Игроки должны быть сгруппированы по командам в соответствующие поля 
-    // объекта fbPlayersByTeams. Под {} в примере имеется ввиду полный объект игрока (такая запись сделана для упрощенного понимания примера)
+const fbTeam = [
+  {
+    playerName: "Messi",
+    team: "Barcelona",
+  },
+  {
+    playerName: "Suarez",
+    team: "Barcelona",
+  },
+  {
+    playerName: "Ronaldo",
+    team: "Juventus",
+  },
+  {
+    playerName: "Buffon",
+    team: "Juventus",
+  },
+  {
+    playerName: "Valerchik",
+    team: "Vedrich97",
+  },
+  {
+    playerName: "Gonsalo",
+    team: "Juventus",
+  },
+  {
+    playerName: "Pedro",
+    team: "Chelsea",
+  },
+  {
+    playerName: "Zuma",
+    team: "Chelsea",
+  },
+  {
+    playerName: "Rico",
+    team: "PSG ",
+  },
+];
 
+// С помощью метода reduce() преобразуйте его в объект вида:
 
+// const fbPlayersByTeams = {
+//     Juventus: [{}, {}, {}],
+//     PSG: [{}],
+//     Chelsea: [{}, {}],
+//     Vedrich97: [{}],
+//     Barcelona: [{}, {}]
+//  }
+// Игроки должны быть сгруппированы по командам в соответствующие поля
+// объекта fbPlayersByTeams. Под {} в примере имеется ввиду полный объект игрока (такая запись сделана для упрощенного понимания примера)
 
-    let res4 = fbTeam.reduce((accu,item) => {
-    if (accu[item.team] === undefined) {
-      accu[item.team] = [item.playerName];
-      
-  } else {
-      accu[item.team].push(item.playerName);
-  }
+let teamPlayers = fbTeam.reduce((accu, { playerName, team }) => {
+  accu[team] = [...(accu[team] || []), playerName];
   return accu;
-            
-    }, {})
+}, {});
 
-
-    function makeToogleMeFunc(){
-      let boo =  0;
-      return function (num){
-        boo++
-            if(num !== undefined){
-               boo = num
-            }
-           
-          return boo
-      }
-      
-  }
-  let toogleMe = makeToogleMeFunc();
-  
-  console.log(toogleMe());
-  console.log(toogleMe());
-  console.log(toogleMe());
-  console.log(toogleMe(10));
-  console.log(toogleMe(10));
-
-
-
-  // 3.
-// Создать функцию classNameEditor(), задача которой "управлять" css-классами. Функция должна принимать строку с названием css-класса и булево значние. Если булево значение true, то функция должна добавлять переданный css-класс, если false, то найти и удалить значение из css-класса. Функция всегда должна возвращать текущее значение css-класса.
-
-// classNameEditor() // '' - вернется пустая строка
-// classNameEditor('test', true) // 'test' - добавили класс 'test'
-// classNameEditor('test', true) // 'test' - попытались добавить класс 'test' еще раз (не должно быть дублирующих классов)
-// classNameEditor('smth', true) // 'test smth' - добавили класс 'smth'
-// classNameEditor('test', false) // 'smth' - удалили класс 'test'
-// classNameEditor('test', false) // 'smth' - удалили класс 'test' еще раз (это невозможно тк класс уже удален)
-
-
-
-        
+console.log(teamPlayers);
